9e43a73f0fe67cdc82ae5d26b417f7290ae04ef1
baysetyo13 <86050760+baysetyo13@users.noreply.github.com>
2022-02-06 19:27:33 +0700

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -77,7 +77,7 @@ def absa_metrics_fn(list_hyp, list_label):
     list_label = list(itertools.chain.from_iterable(list_label))
     
     metrics = {}
-    metrics["ACC"] = accuracy_score(list_label, list_hyp, zero_division=1)
+    metrics["ACC"] = accuracy_score(list_label, list_hyp)
     metrics["F1"] = f1_score(list_label, list_hyp, average='macro', zero_division=1)
     metrics["REC"] = recall_score(list_label, list_hyp, average='macro', zero_division=1)
     metrics["PRE"] = precision_score(list_label, list_hyp, average='macro', zero_division=1)
